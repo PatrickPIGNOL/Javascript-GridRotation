d99d2d924f8a1038aa043da27c45f6224c9b3968
Patrick PIGNOL <patrick.pignol@gmail.com>
2021-03-08 01:28:17 +0100

    Update GameMap.js

--- public/GameMap.js
+++ public/GameMap.js
@@ -40,7 +40,7 @@ export class GameMap extends KeyboardFocusable
 		};
 		this.aMaxCounts =
 		{
-			Dig: Math.floor((this.aMap.Width - 2) * (this.aSize.Height - 2) * this.aPercentages.Dig / 100)
+			Dig: Math.floor((this.aMap.Width - 2) * (this.aMap.Height - 2) * this.aPercentages.Dig / 100)
 		};
 		this.aMap = new Array();
 	}
